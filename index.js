ce7995ffeba604b324bf201f5234dc033885ad3a
GSuleh <31475817+GSuleh@users.noreply.github.com>
2021-11-12 00:18:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,26 +4,28 @@
 /**
  * Fetch list of transactions from the Stellar network.
  */
- var server = new StellarSdk.Server("https://horizon.stellar.org");
-​
- var handleFetchTransactions = () => {
-     console.log("Fetching transactions...");
-     let trxList = document.getElementById("transactions");
-     server
-         .transactions()
-         .call()
-         .then(function (resp) {
-             let transactions = resp.records;
-             console.log(transactions);
-            transactions.map(transaction => {
-                trxList.innerHTML += `<tr><td>${transaction.id}</td><td>${transaction.created_at}</td></td>`;
-            })
+ var server = new StellarSdk.Server("https://horizon.stellar.org");  
+
+ document.getElementById("fetchBtn").addEventListener("click", handleFetchTransactions = () => {
+    console.log("Fetching transactions...");
+    let trxList = document.getElementById("transactions");
+    server
+        .transactions()
+        .call()
+        .then(function (resp) {
+            let transactions = resp.records;
+            console.log(transactions);
+           transactions.map(transaction => {
+            trxList.innerHTML += `<tr><td>${transaction.id}</td><td>${transaction.created_at}</td></td>`;
+           })
+
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+    
+});
+
  
-         })
-         .catch(function (err) {
-             console.error(err);
-         });
-     
- }
  
 
